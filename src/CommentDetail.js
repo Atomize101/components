30b0afb2f2472c2f85d7953170fc04ac04ac108f
Christopher Patrick <cpatrick@nabancard.com>
2019-01-22 17:43:15 -0500

    Wrapping child components with parent component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -5,16 +5,16 @@ const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={props.imgSrc} />
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {props.author}
                     </a>
                 <div className="metadata">
-                    <span className="Date">Today at 6:00 PM</span>
+                    <span className="Date">{props.timeAgo}</span>
                 </div>
-                <div className="text">{faker.lorem.paragraph()}}</div>
+                <div className="text">{props.content}}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
           
-            <CommentDetail author="Chris" />
-            <CommentDetail author="Sarah"/>          
-            <CommentDetail author="Bob"/>           
+            <ApprovalCard>
+                <CommentDetail author="Chris"
+                    timeAgo="Today at 2:00 AM" 
+                    content={faker.lorem.paragraph()}
+                    imgSrc={faker.image.avatar()} />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail author="Sarah" 
+                    timeAgo="Today at 6:00 AM" 
+                    content={faker.lorem.paragraph()}
+                    imgSrc={faker.image.avatar()} />          
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail author="Bob" 
+                    timeAgo="Today at 4:00 PM" 
+                    content={faker.lorem.paragraph()}
+                    imgSrc={faker.image.avatar()} />           
+            </ApprovalCard>
         </div>
     );
 }
